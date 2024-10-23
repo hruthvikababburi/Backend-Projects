6201dacf9a29758e6ff85dbee9364bff81905c05
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-10-23 11:13:47 +0530

    created valid secret key

--- MoneyTracker/app.js
+++ MoneyTracker/app.js
@@ -18,6 +18,12 @@ app.use('/users',userRoutes)
 
 app.use('/transactions',transactionsRoutes)
 
+
+//creating secretKey
+//const crypto = require('crypto');
+//const secret = crypto.randomBytes(32).toString('hex'); // Generates a 64-character hex string
+//console.log(secret);
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
