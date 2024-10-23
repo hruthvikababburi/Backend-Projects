51dea5f69f1abb717d4e696777bcfd4a07fe4d92
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-10-23 08:15:23 +0530

    initialized tables in database

--- MoneyTracker/server.js
+++ MoneyTracker/server.js
@@ -1,8 +1,37 @@
-const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database('./databases/expensesTracker.db')
-
-db.serialize(()=>{
-    db.run(
-        `CREATE TABLE IF NOT EXISTS transactions`
-    )
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./databases/expensesTracker.db', (err)=>{
+    if (err){
+        console.error('Error while connecting to database: ',err)
+    }
+    else{
+        console.log('Connected to database')
+    }
 })
+
+const initializeTables=()=>{
+    db.serialize(()=>{
+        db.run(
+            `CREATE TABLE IF NOT EXISTS transactions(
+            id TEXT PRIMARY KEY,
+            type TEXT NOT NULL,
+            category TEXT NOT NULL,
+            amount REAL NOT NULL,
+            date TEXT NOT NULL,
+            description TEXT NOT NULL
+            )`
+        )
+
+        db.run(
+            `CREATE TABLE IF NOT EXISTS categories(
+            id TEXT PRIMARY KEY,
+            name TEXT NOT NULL,
+            type TEXT NOT NULL
+            )`
+        )             
+    })
+}
+
+module.exports = {
+    db,
+    initializeTables
+}
